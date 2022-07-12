edab64ac347b6069967961ff4ea708976e8d8e86
Kowtham <kowthamravi@outlook.com>
2022-07-12 14:14:41 -0400

    enter

--- Python-Practice/pp16.py
+++ Python-Practice/pp16.py
@@ -1,1 +1,17 @@
-# test
+#PP16 - Password Generator
+from random import randint
+import random 
+import string
+
+def password():
+    uppercase=string.ascii_uppercase
+    print(random.choice(uppercase))
+    lowercase=string.ascii_lowercase
+    print(random.choice(lowercase))
+    num=randint(0,9)
+    print(str(num))
+    symbol=string.punctuation
+    print(random.choice(symbol))
+    
+password()
+
